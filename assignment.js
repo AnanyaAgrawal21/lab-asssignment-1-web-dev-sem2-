cdb89ffd2706d0df6a18ea6f228a7d87e5929387
unknown <an50an50ya@gmail.com>
2026-02-11 11:34:18 +0530

    first commit

---
+++ assignment.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('.form')
+const eventCards = document.querySelector('.cards')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const title = document.querySelector('#eventTitle').value
+  const date = document.querySelector('#eventDate').value
+  const cat = document.querySelector('#category').value
+  const desc = document.querySelector('#description').value
+
+  const card = document.createElement('div')
+  card.classList.add('card')
+
+  card.innerHTML = `
+    <div class="dlt">×</div>
+    <h3>${title}</h3>
+    <p>${date}</p>
+    <button>${cat}</button>
+    <p>${desc}</p>
+  `
+
+  eventCards.appendChild(card)
+
+
+  card.querySelector('.dlt').addEventListener('click', () => {
+    card.remove()
+  })
+
+  form.reset()
+})
+
+
+document.querySelector('#clearAll').addEventListener('click', () => {
+  eventCards.innerHTML = ''
+})
+
+"C:\Users\alend\OneDrive\lab assignment 1 web"
